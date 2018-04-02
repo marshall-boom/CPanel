117a0bfd704e5451110cfe836112a694281b9944
ddmarshall <ddmarshall@gmail.com>
2018-04-02 16:20:47 -0700

    Added licenses to the octree portion of code.

--- octree/tests/octree_test.cpp
+++ octree/tests/octree_test.cpp
@@ -1,13 +1,19 @@
-//
-//  main.cpp
-//  OctreeUnitTests
-//
-//  Created by Chris Satterwhite on 4/15/14.
-//  Copyright (c) 2014 Chris Satterwhite. All rights reserved.
-//
-
-//#include "OctreeTests.h"
-//#include "NodeTests.h"
+/*******************************************************************************
+ * Copyright (c) 2014 Chris Satterwhite
+ * Copyright (c) 2018 David D. Marshall <ddmarsha@calpoly.edu>
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * See LICENSE.md file in the project root for full license information.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Chris Satterwhite - initial code and implementation
+ *    David D. Marshall - porting to GoogleTest
+ ******************************************************************************/
 
 #include <array>
 #include <memory>
